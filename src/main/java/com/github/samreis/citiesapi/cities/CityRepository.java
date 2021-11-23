69782c4dc65f2fd944d5e2b7d70122a2bb20b48f
Samantha Reis <smthalvesreis@gmail.com>
2021-11-23 02:08:15 -0300

    Adiciona projeto cities-api

---
+++ src/main/java/com/github/samreis/citiesapi/cities/CityRepository.java
@@ -0,0 +1,8 @@
+package com.github.samreis.citiesapi.cities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+
+}
+
---
+++ src/main/java/com/github/samreis/citiesapi/staties/StateRepository.java
@@ -0,0 +1,7 @@
+package com.github.samreis.citiesapi.staties;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+
+}
